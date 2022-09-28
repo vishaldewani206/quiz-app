3111b0df7b75de85fe1de8e8449bc1e0a3efbbc5
Vishal <dewani.vishal206@gmail.com>
2022-09-28 15:46:15 +0500

    everything completed in home page

--- app.js
+++ app.js
@@ -80,3 +80,11 @@ checkBtn.addEventListener('click', () => {
     }
   }
 });
+
+//Hamburger
+let hamburger = document.querySelector('.hamburger');
+let navLinks = document.querySelector('.nav-links');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('nav-active');
+});
